64cccf86f9c48092647566a6d09325dfe332a8d5
himaloyash <himaloyashis98@gmail.com>
2024-01-02 16:23:03 +0600

    dropdown prblm solved

---
+++ src/app/dropdownprblm/page.jsx
@@ -0,0 +1,45 @@
+"use client"
+import React, { useState } from 'react';
+
+const Dropdown = () => {
+    const [country, setCountry] = useState(0)
+    console.log(country);
+
+    const countries = [
+        {
+            name: "india", value: "IN", cities: ["Kolkata", "Delhi"]
+        },
+        {
+            name: "Nepal", value: "NP", cities: ["Kathmandu", "Selong"]
+        },
+        {
+            name: "Bangladesh", value: "BN", cities: ["Dhaka", "Chittagong"]
+        }
+    ]
+
+
+    return (
+        <div>
+            <select value={country} onChange={(e) => setCountry(e.target.value)}
+
+            >
+                {
+                    countries.map((item, index) => {
+                        return (<option value={index} key={index}> {item.name}</option>)
+                    })
+                }
+
+            </select>
+
+            <select value={country} >
+                {
+                    countries[country].cities.map((item, index) => {
+                        return <option value={index} key={index}>{item} </option>
+                    })
+                }
+            </select>
+        </div>
+    );
+};
+
+export default Dropdown;
